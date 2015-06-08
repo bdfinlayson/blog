857961c026d4f9c9874380514c625525cd269949
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-08 17:00:56 -0500

    Add devise login logout sign up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.all.order('created_at DESC')
   end
